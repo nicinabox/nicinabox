514a94ceff1b2da1cb0b2b4228dbafb98226025b
Nic Aitch <nic@nicinabox.com>
2015-08-19 18:17:53 -0500

    Close drawers across groups

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,20 @@ import $ from 'webpack-zepto';
 
 fastclick.attach(document.body);
 
-var selector = '.project-container';
+var container = '.project-container';
+var group = '.project-group';
 
-$(document).on('click', selector, (e) => {
+$(document).on('click', container, (e) => {
   var $link = $(e.target).closest('a');
   if ($link.length && !$(e.target).closest('a').hasClass('details')) return;
   e.preventDefault();
 
   $(e.currentTarget).siblings().removeClass('active');
+  $(e.currentTarget).parents(group).siblings().find(container).removeClass('active');
   $(e.currentTarget).toggleClass('active');
 });
 
 $(document).on('click', (e) => {
-  if ($(e.target).closest(selector).length) return;
-  $(selector).removeClass('active');
+  if ($(e.target).closest(container).length) return;
+  $(container).removeClass('active');
 });
