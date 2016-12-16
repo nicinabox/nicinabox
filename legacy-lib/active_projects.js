211dac2dd0f020daf14ac29e9384cf78f986d8e6
Nic Aitch <nic@nicinabox.com>
2016-12-16 11:52:41 -0600

    Bye legacy files

--- legacy-lib/active_projects.js
+++
@@ -1,6 +0,0 @@
-import recentRepos from './recent_repos';
-
-export default function(repos) {
-  return recentRepos(repos, 30)
-    .filter((repo) => repo.isOwner);
-};
--- legacy-lib/recent_contributions.js
+++
@@ -1,6 +0,0 @@
-import { reject } from 'lodash';
-import recentRepos from './recent_repos';
-
-export default function(repos) {
-  return reject(recentRepos(repos, 365), (project) => !project.isFork);
-};
