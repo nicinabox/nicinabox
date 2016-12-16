3d282e7e6ce3881e7fd7dd8ecc509d07b8d5a4c0
Nic Aitch <nic@nicinabox.com>
2016-12-16 11:58:28 -0600

    Format date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { format } from 'date-fns'
 
 export default function Footer (props) {
   return (
     <div id="footer">
       <p className="text-muted">
-        Built on <strong>{props.lastBuild.toString()}</strong>.{' '}
+        Built on <strong>{format(props.lastBuild, 'dddd, MMMM Do, YYYY')}</strong>.{' '}
         Curious how this site was made?{' '}
         <a href="https://github.com/nicinabox/nicinabox">Check out the source.</a>
       </p>
