d1ec8665594d1250e83d32b0e16e10c5ce5f25f0
Nic Haynes <nic@nicinabox.com>
2020-06-21 12:20:17 -0500

    Update watch files

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = config => {
     config.addPassthroughCopy("src/images");
     config.addPassthroughCopy("src/client.js");
 
+    config.addWatchTarget("./src/*.js");
+    config.addWatchTarget("./src/styles/*.scss");
+    config.addWatchTarget("./src/_data/*.json");
+
     return {
         port: 8080,
         dir: {
