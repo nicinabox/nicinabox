4905a5f73753a79d156e4e87fd0aa908365f67ec
Nic Haynes <nic@nicinabox.com>
2020-06-21 10:35:29 -0500

    Update styles

--- .eleventy.js
+++ .eleventy.js
@@ -5,9 +5,9 @@ module.exports = config => {
     return {
         port: 8080,
         dir: {
-          input: "src",
-          output: "build",
-          data: "_data",
+            input: "src",
+            output: "build",
+            data: "_data",
         },
     }
 };
